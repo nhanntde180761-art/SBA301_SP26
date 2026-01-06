d686f88fff2ba94bb9ebf0cf8fbbeca1b0ae3c17
nhan <nhannguyen.070704@gmail.com>
2026-01-06 12:19:42 +0700

    fisrt commit

---
+++ Slot1/ex1/src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header>
+      <h1>Xin chào</h1>
+      <button style={{ backgroundColor: 'deepskyblue', color: 'white' }}>
+       Click Me
+      </button>
+    </header>
+  )
+}
+
+export default Header;
