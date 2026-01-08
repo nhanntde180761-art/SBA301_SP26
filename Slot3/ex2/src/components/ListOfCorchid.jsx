dd0b1c336aedd6aaadd3bb6e0ee58def0e6b2403
nhan <nhannguyen.070704@gmail.com>
2026-01-08 11:33:57 +0700

    commit

---
+++ Slot3/ex2/src/components/ListOfCorchid.jsx
@@ -0,0 +1,25 @@
+import { Container, Row, Col, Card } from 'react-bootstrap'
+
+function ListOfCorchid({ orchid }) {
+  return (
+    <Container className="mt-3">
+      <Row>
+        {orchid.map(o => (
+          <Col md={4} key={o.id} className="mb-3">
+            <Card>
+              <Card.Img variant="top" src={o.image} />
+              <Card.Body>
+                <Card.Title>{o.orchidName}</Card.Title>
+                <Card.Text>{o.description}</Card.Text>
+                <p>price: ${o.price}</p>
+                {o.isSpecial && <span className="badge bg-danger">special</span>}
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  )
+}
+
+export default ListOfCorchid
